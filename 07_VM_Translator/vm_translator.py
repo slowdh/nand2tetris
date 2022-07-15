c98209d51c42150cc9a2f830b7448b10d374861c
slowdh <leok0909@snu.ac.kr>
2022-07-15 18:22:21 +0900

    add VM Translator: memory operation, arithmetic operation, I/O

--- 07_VM_Translator/vm_translator.py
+++ 07_VM_Translator/vm_translator.py
@@ -31,45 +31,124 @@ class Parser:
 
 
 class Translator:
-    def translate_arithmetic(self, operation):
-        if operation == 'add':
-            pass
-        elif operation == 'sub':
-            pass
-        elif operation == 'neg':
-            pass
-        elif operation == 'eq':
-            pass
-        elif operation == 'gt':
-            pass
-        elif operation == 'lt':
-            pass
-        elif operation == 'and':
-            pass
-        elif operation == 'or':
-            pass
-        elif operation == 'not':
-            pass
+    def translate_arithmetic_op(self, operation):
+        if operation in ('not', 'neg'):  # one value operation
+            assembly_output = [
+                '@SP',
+                'A=M-1'
+            ]
+            if operation == 'not':
+                assembly_output.append('M=!M')
+            else:  # operation == 'neg'
+                assembly_output.append('M=-M')
+        else:  # two value operation
+            assembly_output = [
+                '@SP',
+                'M=M-1',
+                'A=M-1',
+                'D=M',
+                '@SP',
+                'A=M'
+            ]
+            if operation == 'add':
+                assembly_output.append('D=D+A')
+            elif operation == 'sub':
+                assembly_output.append('D=D-A')
+            elif operation == 'eq':
+                pass
+            elif operation == 'gt':
+                pass
+            elif operation == 'lt':
+                pass
+            elif operation == 'and':
+                assembly_output.append('D=D&A')
+            elif operation == 'or':
+                assembly_output.append('D=D|A')
+            else:
+                raise NotImplementedError(f"operation {operation} is not implemented.")
+
+            assembly_output += [
+                '@SP',
+                'A=M-1',
+                'M=D'
+            ]
+        return '\n'.join(assembly_output)
+
+    @staticmethod
+    def translate_memory_op(operation, memory_segment, address):
+        if operation == 'push':
+            if memory_segment == 'constant':
+                asm_data = [
+                    f'@{address}',
+                    'D=A'
+                ]
+            else:
+                asm_data = [
+                    f'@{address}',
+                    'D=A',
+                    f'@{memory_segment}'
+                    'A=D+M',
+                    'D=M'
+                ]
+
+            asm_push = [
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1'
+            ]
+            asm_output = '\n'.join(asm_data + asm_push)
         else:
-            raise NotImplementedError(f"operation {operation} is not implemented.")
+            assert operation == 'pop'
+            assert memory_segment != 'constant'
+            asm_pop = [
+                '@SP',
+                'AM=M-1',
+                f'@{memory_segment}',
+                'D=M',
+                f'@{address}',
+                'D=D+A',
+                f'@TEMP',
+                'M=D',
+                '@SP',
+                'D=M',
+                '@TEMP',
+                'A=M',
+                'A=D'
+            ]
+            asm_output = '\n'.join(asm_pop)
+        return asm_output
 
 
 class VMtranslator:
     def __init__(self):
         self.file_name = None
 
-    def read_file(self, file_path):
+    def translate(self, file_path, debug=False):
         self.file_name = file_path.split('/')[-1].split('.')[0]
-
+        parser = Parser()
+        translator = Translator()
+        save_path = file_path.replace('.vm', '.asm')
         with open(file_path, 'r') as rf:
-            for line in rf:
-                # Parse input
-                # Write output
-                print(line, end='')
+            with open(save_path, 'w') as wf:
+                for line in rf:
+                    parser.parse_line(line)
+                    if parser.operation_type == 'compute':
+                        asm_line = translator.translate_arithmetic_op(parser.op_code)
+                    elif parser.operation_type == 'memory':
+                        asm_line = translator.translate_memory_op(parser.op_code, parser.memory_segment, parser.address)
+                    else:  # == 'comment'
+                        continue
+                    wf.write(asm_line + '\n')
+
+        if debug:
+            with open(save_path, 'r') as f:
+                print(f.read())
 
 
 if __name__ == '__main__':
-    path = './test/SimpleAdd.vm'
+    path = './test/StaticTest.vm'
 
-    translator = VMtranslator()
-    translator.read_file(path)
+    vm_translator = VMtranslator()
+    vm_translator.translate(path, debug=True)
